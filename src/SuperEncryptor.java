1ff01b1339b0922fa6fe2354a45cefc4eac208f7
tyler <ttang7910@bths.edu>
2023-03-12 15:53:48 -0400

    created most of the helper methods (didn't create the fill box method for decryption yet)

--- src/SuperEncryptor.java
+++ src/SuperEncryptor.java
@@ -1,13 +1,19 @@
+import java.util.ArrayList;
+
 public class SuperEncryptor {
     private String[][] box;
     private int rows;
     private int cols;
-    private int shift;
+    private int shiftA;
+    private int shiftR;
+    private int shiftC;
 
-    public SuperEncryptor(int r, int c, int shift) {
+    public SuperEncryptor(int r, int c, int sA, int sR, int sC) {
         rows = r;
         cols = c;
-        this.shift = shift;
+        shiftA = sA;
+        shiftR = sR;
+        shiftC = sC;
     }
 
     public void fillBlock(String str)
@@ -39,13 +45,13 @@ public class SuperEncryptor {
 
             if(capital.contains(letter)) {
                 int indx = capital.indexOf(letter);
-                indx += shift;
+                indx += shiftA;
 
                 newLetter = capital.charAt(indx % 26) + "";
 
             } else if (lowercase.contains(letter)) {
                 int indx = lowercase.indexOf(letter);
-                indx += shift;
+                indx += shiftA;
 
                 newLetter = lowercase.charAt(indx % 26) + "";
 
@@ -71,13 +77,13 @@ public class SuperEncryptor {
 
             if(capital.contains(letter)) {
                 int indx = capital.indexOf(letter);
-                indx += shift;
+                indx += shiftA;
 
                 newLetter = capital.charAt(indx % 26) + "";
 
             } else if (lowercase.contains(letter)) {
                 int indx = lowercase.indexOf(letter);
-                indx += shift;
+                indx += shiftA;
 
                 newLetter = lowercase.charAt(indx % 26) + "";
 
@@ -89,6 +95,70 @@ public class SuperEncryptor {
         }
 
         return scramble;
+    }
+
+    public String[][] shiftRows(int shiftFactor) {
+        shiftFactor %= rows;
+
+        for(int i = 0; i < shiftFactor; i++) {
+            String[] temp = box[0].clone();
+
+            for(int x = 0; x < box.length - 1; x++) {
+                box[x] = box[x + 1].clone();
+            }
+            box[box.length - 1] = temp;
+        }
+
+        return box;
+    }
+
+    public String[][] unshiftRows() {
+        int unshift = rows - (shiftR % rows);
+
+        return shiftRows(unshift);
+    }
+
+    public String[][] shiftCol(int shiftFactor) {
+        shiftFactor %= cols;
+
+        ArrayList<String[]> differentCol = new ArrayList<>();
+        String[] tempCol = new String[box.length];
+
+        for(int c = 0; c < box[0].length; c++) {
+            for(int r = 0; r < box.length; r++) {
+                tempCol[r] = box[r][c];
+            }
+            differentCol.add(tempCol.clone());
+        }
+
+        for(int i = 0; i < shiftFactor; i++) {
+            differentCol.add(differentCol.remove(0));
+        }
+
+        for(int c = 0; c < differentCol.size(); c++) {
+            for(int r = 0; r < box.length; r++) {
+                String[] temp = differentCol.get(c);
+
+                box[r][c] = temp[r];
+            }
+        }
+
+        return box;
+    }
+
+    public String[][] unshiftCol() {
+        int unshiftFactor = cols - (shiftC % cols);
+
+        return shiftCol(unshiftFactor);
+
+    }
+
+    public void setBox(String[][] b) {
+        box = b;
+    }
+
+    public String[][] getBox() {
+        return box;
     }
 
 }
--- src/SuperTester.java
+++ src/SuperTester.java
@@ -1,12 +1,44 @@
+import java.util.Arrays;
+
 public class SuperTester {
     public static void main(String[] args) {
 
-        SuperEncryptor s = new SuperEncryptor(5, 5, 2);
+        int shiftR = 2;
+        int shiftC = 10;
+
+        SuperEncryptor s = new SuperEncryptor(4, 3, 2, shiftR, shiftC);
 
         String test = s.shiftLetters("Heloo dueede");
 
-        System.out.println(test);
-        System.out.println(s.unshiftLetters(test));
+        String[][] testB = {{"H", "E", "L"},
+                            {"L", "L", "O"},
+                            {" ", "D", "U"},
+                            {"D", "E", "-"}};
 
+        s.setBox(testB);
+
+        /*
+        printArray(s.shiftRows(shiftR));
+        System.out.println();
+        printArray(s.unshiftRows());
+
+         */
+
+
+        printArray(s.shiftCol(shiftC));
+        System.out.println();
+        printArray(s.unshiftCol());
+
+
+
+    }
+
+    public static void printArray(String[][] arr) {
+        for(int r = 0; r < arr.length; r++) {
+            for(int c = 0; c < arr[0].length; c++) {
+                System.out.print(arr[r][c] + " ");
+            }
+            System.out.println();
+        }
     }
 }
